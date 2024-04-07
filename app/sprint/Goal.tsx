a0332aeef96c13cfec3dd12d028f986e60dcba9c
Miguel Guerrero <miguel_angel_g@live.com>
2024-04-07 12:59:54 -0400

    fix: turning goal into a component wrapped by Suspense

--- app/sprint/Goal.tsx
+++ app/sprint/Goal.tsx
@@ -1,1 +1,10 @@
 'use client';
+import { useSearchParams } from 'next/navigation';
+
+export default function Goal() {
+  const goal =
+    useSearchParams().get('goal') ||
+    '"A journey of a thousand miles begins with a single step."';
+
+  return <h1 className='p-8 text-4xl'>{goal}</h1>;
+}
--- app/sprint/page.tsx
+++ app/sprint/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
+import Goal from '@/app/sprint/Goal';
 
 export default function Page() {
-  const goal =
-    useSearchParams().get('goal') ||
-    '"A journey of a thousand miles begins with a single step."';
-
   return (
-    <Suspense fallback='...'>
-      <section className='container flex min-h-dvh flex-col place-content-center place-items-center'>
-        <h1 className='p-8 text-4xl'>{goal}</h1>
-      </section>
-    </Suspense>
+    <section className='container flex min-h-dvh flex-col place-content-center place-items-center'>
+      <Suspense>
+        <Goal></Goal>
+      </Suspense>
+    </section>
   );
 }
