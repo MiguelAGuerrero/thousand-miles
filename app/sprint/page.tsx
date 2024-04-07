c12f332ccd2484f9f1da012208e995a28d4b00bd
Miguel Guerrero <miguel_angel_g@live.com>
2024-04-07 12:24:19 -0400

    feat: input a goal

--- app/sprint/page.tsx
+++ app/sprint/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function Page() {
-  const goal = useSearchParams().get('goal') || 'No goal set';
+  const goal =
+    useSearchParams().get('goal') ||
+    '"A journey of a thousand miles begins with a single step."';
 
   return (
     <section className='container flex min-h-dvh flex-col place-content-center place-items-center'>
-      <h1 className='p-8 text-xl'>{goal}</h1>
+      <Suspense fallback='...'>
+        <h1 className='p-8 text-4xl'>{goal}</h1>
+      </Suspense>
     </section>
   );
 }
